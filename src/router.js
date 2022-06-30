7500f29d20840713a0280e2232ff72477b18cc61
Ronald Davisson <ronaldadavisson@gmail.com>
2022-06-29 20:52:00 -0700

    v_111

--- src/router.js
+++ src/router.js
@@ -1,24 +1,24 @@
-import { createWebHistory, createRouter } from "vue-router";
-const routes =  [
-  {
-    path: "/",
-    alias: "/tasks",
-    name: "tasks",
-    component: () => import("./components/TaskList")
-  },
-  {
-    path: "/tasks/:id",
-    name: "task-details",
-    component: () => import("./components/Task")
-  },
-  {
-    path: "/add",
-    name: "add",
-    component: () => import("./components/AddTask")
-  }
-];
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
+import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+export default new Router({
+  //mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/tasks",
+      name: "tasks",
+      component: () => import("./components/TasksList")
+    },
+    {
+      path: "/tasks/:id",
+      name: "task-details",
+      component: () => import("./components/Task")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/AddTask")
+    }
+  ]
 });
-export default router;
