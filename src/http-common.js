e1fcb3ee6fe456ce352effebfdcd1076ff8a7ec6
Ronald Davisson <ronaldadavisson@gmail.com>
2022-07-01 11:09:30 -0700

    v_407

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "postgres://postgres:NhRjPqTCgO4dB6XijoMhrXYRGtIaRU8I@dpg-capo44v6d9kiqtaa2920-a.oregon-postgres.render.com/postgres",
+  baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json"
   }
