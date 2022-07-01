d63dac8b1460e77fcb86980509602a77f8e804e7
Ronald Davisson <ronaldadavisson@gmail.com>
2022-07-01 09:41:08 -0700

    v_400

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://apitasks.onrender.com/api",
+  baseURL: "postgres://admin:NhRjPqTCgO4dB6XijoMhrXYRGtIaRU8I@dpg-capo44v6d9kiqtaa2920-a.oregon-postgres.render.com/tasks_lmgp",
   headers: {
     "Content-type": "application/json"
   }
